9704da623453cc996b4adb13a4de74edcf5b6788
akiaxin <142460593+akiaxin@users.noreply.github.com>
2024-02-28 12:37:04 -0700

    Add files via upload

---
+++ src/character.py
@@ -0,0 +1,18 @@
+# CS
+class Character:
+  def __init__(self, name, health, attack):
+      self.name = name
+      self.health = health
+      self.attack = attack
+
+  def is_alive(self):
+      return self.health > 0 
+
+class Player(Character):
+  def __init__(self, name):
+    super().__init__(name, health=100, attack=10)
+    self.inventory = []
+
+class Enemy(Character):
+  def __init__(self, name, health, attack):
+      super().__init__(name, health, attack)
