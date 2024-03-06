4e903056cbe51cfe9d83766d4fd5a585d1943199
akiaxin <142460593+akiaxin@users.noreply.github.com>
2024-03-06 11:31:29 -0700

    Add files via upload

---
+++ src/Shop.py
@@ -0,0 +1,10 @@
+class Shop:
+  # Variables
+   goodNum = []
+
+  # Constructor
+  def __init__(self, damageMult, ammo, name, price):
+    self.damageMult = damageMult
+    self.ammo = ammo
+    self.name = name
+    self.price = price
