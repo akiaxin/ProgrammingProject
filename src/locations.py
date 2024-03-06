dca64aeb19bc00ff9cc3dc8b29c956c719e2012e
akiaxin <142460593+akiaxin@users.noreply.github.com>
2024-03-06 11:30:34 -0700

    Delete src directory

--- src/locations.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def explore():
-  locations = ["your mom's basement", "under the living room couch", 
-               "an abandoned castle", "a dark forest"]
-
-  location = random.choice(locations)
-  print("You enter " + location)
-  
